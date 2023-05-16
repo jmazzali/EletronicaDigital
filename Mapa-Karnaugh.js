4d246298445a3c262441c3dcbe67a0f368a1e38d
jmazzali <joaovitormazzali@gmail.com>
2023-05-16 20:08:27 -0300

    Refazendo mapa de karnaugh e adicionando comentários

--- Mapa-Karnaugh.js
+++ Mapa-Karnaugh.js
@@ -1,3 +1,29 @@
+/*
+ANOTAÇÕES:
+O mapa de karnaugh tem como funcionalidade auxiliar nas reduções de funções booleanas.
+É citado como mapa pois se assemelha a um campo minado, onde cada "quadrado" representa uma possibilidade
+Um mapa de 2 variáveis, por exemplo, deve possuir 4 quadrados visto que 2 variáveis em binários podem formar uma variedade de 4 possibilidades
+Possibilidades = 2(base binária) ^ qtd de variáveis
+
+Neste mesmo exemplo, teríamos as colunas como uma variável e as linhas como outra variável, sua visualização seria:
+
+    A'  |  A
+B'| 00  | 10
+B | 01  | 11
+
+A variável sozinha indica que seu valor lógico é alto, enquanto a negada (A' e B') são considerados baixo valor lógico
+
+Para inserir funções no mapa, pode-se considerar 2 métodos: a soma de produtos (mais frequente) ou o produto de somas
+Quando uma possibilidade de entrada da funcao tem saida de valor logico alto, insere-se 1 naquele "quadrado"
+Enquanto para saidas de baixo valor, insere-se 0
+
+Na soma de produtos (mais frequente), a funcao e definida pela soma dos produtos (um produto e o quadrado que possui 1)
+
+Segue-se a seguinte lógicas para simplificação:
+DUPLA: remove uma variável
+QUADRA: remove duas variáveis
+OCTETO: remove três variáveis
+*/
 function MapaKarnaugh(QTDEntradas)
 {
     let colunas, linhas
@@ -5,77 +31,103 @@ function MapaKarnaugh(QTDEntradas)
     {
         colunas = 4
         linhas = 4
+        let A, B, C, D
     }
     else if(QTDEntradas == 3)
     {
         colunas = 2
         linhas = 4
+        let A, B, C
     }
     else if(QTDEntradas == 2)
     {
         colunas = 2
-        linhas =2
+        linhas = 2
+        let A, B
     }
-    let aux = 0
-    let variavelESQ = ''
-    let variavelDIR = ''
-    for (let linha = 1; linha <= linhas; linha++) 
+
+    for (let linha = 0; linha <= (linhas-1); linha++) 
     {
         let linhaAtual = ''
-        for (let coluna = 1; coluna <= colunas; coluna++)
+        if(linhas == 2)//se 2 linhas, elas serão destinadas a uma variável, no caso B e B'
         {
-            if (linha == 1)
+            if(linha == 0)//na primeira linha usar B'
             {
-                if(aux == 0)
-                {
-                    if(colunas == 2)
-                    {
-                        console.log("     A   |  A'")
-                        console.log("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
-                        aux = 1
-                    }
-                    else if (colunas == 4)
-                    {
-                        console.log("        A      |      A'")
-                        console.log("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
-                        aux = 1
-                    }
-                }
-                variavelESQ = "B "
-                if (linhas > 2)
-                variavelDIR = "C "
+                B = 0;
             }
-            if (linha == 2)
-                variavelESQ = "B'"
-            if (linha == 3 && linhas > 2)
-                variavelDIR = "C'"
-            if (linha == 4)
-                variavelESQ = "B "
-
-            /*if(linha != 1 && colunas == 2)
+            else if(linha == 1)//na segunda linha usar B
             {
-                if(j==1)
-                {
-                    if(linha == 2)
-                    linhaAtual += '  B  '
-                    else if(linha == 3)
-                    linhaAtual += "  B' "
-                }
-            }*/
-            //if(linha != 1)
-            linhaAtual += '| ' + linha + ',' + coluna + ' '
+                B = 1;
+            }
         }
-        console.log(variavelESQ + " " +linhaAtual + "| " + variavelDIR)
-        if(linha == linhas)
-            if(colunas == 2)
-            console.log("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
-            else if (colunas == 4)
+        else if(linhas == 4)
+        {
+            if(linha == 0 || linha == 1)
             {
-                console.log("_____________________________")
-                console.log("      D  |     D'    |  D")
+                B = 0;
+            }
+            else if(linha == 2 || linha == 3)
+            {
+                B = 1;
             }
 
+            if(linha == 0 || linha == 3)
+            {
+                C = 0;
+            }
+            else if(linha == 1 || linha == 2)
+            {
+                C = 1;
+            }
+        }
+        for (let coluna = 0; coluna <= (colunas-1); coluna++)
+        {
+            if(colunas == 2)//se 2 colunas, nossas colunas serão destinadas a uma variável, no caso A e A'
+            {
+                if(coluna == 0)//se na primeira coluna, usar A'
+                {
+                    A = 0;
+                }
+                else if (coluna == 1)//se na segunda coluna, usar A
+                {
+                    A = 1;
+                }
+            }
+            else if(colunas == 4)//se 4 colunas uma parte será A' e outra A
+            {
+                if(coluna == 1 || coluna == 2)//as duas centrais são A'
+                {
+                    A = 0;
+                }
+                else if(coluna == 0 || coluna == 3)//as duas extremas são A
+                {
+                    A = 1;
+                }
+                //Adicionar a quarta variável aqui, só que intercalar: se as pontas são A' o centro deve ser D
+                //se o centro é A as pontas devem ser D'
+                if(coluna == 0 || coluna == 1)
+                {
+                    D = 0;
+                }
+                else if(coluna == 2 || coluna == 3)
+                {
+                    D = 1;
+                }
+            }
+
+            if(linhas == 2 && colunas == 2)//se for um mapa 2x2
+                linhaAtual += '| ' + B + A + ' '//a saída vai conter só 2 variáveis
+            if(linhas == 4 && colunas == 2)//se for um mapa 4x2
+                linhaAtual += '| ' + B + C + A + ' '
+            if(linhas == 4 && colunas == 4)
+                linhaAtual += '| ' + B + C + A + D + ' '
+        }
+        console.log(linhaAtual + "| ")
     }
 }
 
 MapaKarnaugh(2)
+console.log("")
+MapaKarnaugh(3)
+console.log("")
+MapaKarnaugh(4)
